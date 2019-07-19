876a086cd359b7a91f83df78c624e1a249894399
Apostol <neketapostolov@gmail.com>
2019-07-19 22:45:59 +0300

    GRAPH FIGURES

--- app/src/main/java/com/missclick/mchess/MainActivity.java
+++ app/src/main/java/com/missclick/mchess/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new DrawView(this));
+        Controller controller = new Controller();
+        setContentView(new DrawView(this, controller));
     }
 }
