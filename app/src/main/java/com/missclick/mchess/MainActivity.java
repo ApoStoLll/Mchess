1f9d0d6c73cead5da30c71d217c88890eb7521ea
Apostol <neketapostolov@gmail.com>
2019-07-19 22:51:01 +0300

    empry touch

--- app/src/main/java/com/missclick/mchess/MainActivity.java
+++ app/src/main/java/com/missclick/mchess/MainActivity.java
@@ -2,13 +2,30 @@ package com.missclick.mchess;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    private DrawView drawView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Controller controller = new Controller();
-        setContentView(new DrawView(this, controller));
+        drawView = new DrawView(this, controller);
+        drawView.setOnTouchListener(this);
+        setContentView(drawView);
+    }
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: // нажатие
+
+        }
+        drawView.postInvalidate();
+        return false;
     }
 }
