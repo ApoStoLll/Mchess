64b35f11ce3321e52e0fde87b9b2d8aca61bf1b1
danil.vladyka1 <danil.vladyka1@gmail.com>
2019-07-20 17:26:01 +0300

    movelist for pawn,bishop,rook,queen,knight upd 2.1.4

--- app/src/main/java/com/missclick/mchess/figures.java
+++ app/src/main/java/com/missclick/mchess/figures.java
@@ -173,7 +173,28 @@ class Knight extends figures{
         ArrayList movelist = new ArrayList();
         int x = this.coor.getX();
         int y = this.coor.getY();
-        return checkRook(field,x,y,movelist);
+        if(this.color == 0){
+            if(x+1<8 && y+2<8) if(field[x+1][y+2]>=0) movelist.add(new Coordinate(x+1,y+2));
+            if(x-1>=0 && y+2<8) if(field[x-1][y+2]>=0) movelist.add(new Coordinate(x-1,y+2));
+            if(x+2<8 && y+1<8) if(field[x+2][y+1]>=0) movelist.add(new Coordinate(x+2,y+1));
+            if(x-2>=0 && y+1<8) if(field[x+2][y+1]>=0) movelist.add(new Coordinate(x-2,y+1));
+            if(x+1<8 && y-2>=0) if(field[x+1][y+2]>=0) movelist.add(new Coordinate(x+1,y-2));
+            if(x-1>=0 && y-2>=0) if(field[x-1][y+2]>=0) movelist.add(new Coordinate(x-1,y-2));
+            if(x+2<8 && y-1>=0) if(field[x+2][y+1]>=0) movelist.add(new Coordinate(x+2,y-1));
+            if(x-2>=0 && y-1>=0) if(field[x+2][y+1]>=0) movelist.add(new Coordinate(x-2,y-1));
+
+        }
+        else{
+            if(x+1<8 && y+2<8) if(field[x+1][y+2]<=0) movelist.add(new Coordinate(x+1,y+2));
+            if(x-1>=0 && y+2<8) if(field[x-1][y+2]<=0) movelist.add(new Coordinate(x-1,y+2));
+            if(x+2<8 && y+1<8) if(field[x+2][y+1]<=0) movelist.add(new Coordinate(x+2,y+1));
+            if(x-2>=0 && y+1<8) if(field[x+2][y+1]<=0) movelist.add(new Coordinate(x-2,y+1));
+            if(x+1<8 && y-2>=0) if(field[x+1][y+2]<=0) movelist.add(new Coordinate(x+1,y-2));
+            if(x-1>=0 && y-2>=0) if(field[x-1][y+2]<=0) movelist.add(new Coordinate(x-1,y-2));
+            if(x+2<8 && y-1>=0) if(field[x+2][y+1]<=0) movelist.add(new Coordinate(x+2,y-1));
+            if(x-2>=0 && y-1>=0) if(field[x+2][y+1]<=0) movelist.add(new Coordinate(x-2,y-1));
+        }
+        return movelist;
     }
 }
 
