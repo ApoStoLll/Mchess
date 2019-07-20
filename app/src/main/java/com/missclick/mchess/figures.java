8fd98a84c4fc278c526a59977760e6410981fbf0
danil.vladyka1 <danil.vladyka1@gmail.com>
2019-07-20 17:01:12 +0300

    movelist for pawn,bishop,rook,queen upd 2.1.4

--- app/src/main/java/com/missclick/mchess/figures.java
+++ app/src/main/java/com/missclick/mchess/figures.java
@@ -13,6 +13,84 @@ abstract class figures {
         return null;
     }
     Coordinate getCoor(){ return coor; }
+    ArrayList checkRook(int field[][],int x,int y,ArrayList movelist){
+        for(int i=1;i<8;i++){
+            if(x+i<8){
+                if (field[x+i][y] < 0 && this.color == 0) break;
+                if (field[x+i][y] > 0 && this.color == 1) break;
+                movelist.add(new Coordinate(x + i, y));
+                if (field[x+i][y] != 0) break;
+            }
+            else break;
+        }
+        for(int i=1;i<8;i++){
+            if(x-i>=0){
+                if (field[x-i][y] < 0 && this.color == 0) break;
+                if (field[x-i][y] > 0 && this.color == 1) break;
+                movelist.add(new Coordinate(x - i, y));
+                if (field[x-i][y] != 0) break;
+            }
+            else break;
+        }
+        for(int i=1;i<8;i++){
+            if(y+i<8){
+                if (field[x][y+i] < 0 && this.color == 0) break;
+                if (field[x+i][y+i] > 0 && this.color == 1) break;
+                movelist.add(new Coordinate(x, y+i));
+                if (field[x][y+i] != 0) break;
+            }
+            else break;
+        }
+        for(int i=1;i<8;i++){
+            if(y-i>=0){
+                if (field[x][y-i] < 0 && this.color == 0) break;
+                if (field[x+i][y-i] > 0 && this.color == 1) break;
+                movelist.add(new Coordinate(x, y-i));
+                if (field[x][y-i] != 0) break;
+            }
+            else break;
+        }
+        return movelist;
+    }
+    ArrayList checkBishop(int field[][],int x,int y,ArrayList movelist){
+        for(int i=1;i<8;i++){
+            if(x+i<8 && y+i<8){
+                if (field[x+i][y+i] < 0 && this.color == 0) break;
+                if (field[x+i][y+i] > 0 && this.color == 1) break;
+                movelist.add(new Coordinate(x + i, y + i));
+                if (field[x+i][y+i] != 0) break;
+            }
+            else break;
+        }
+        for(int i=1;i<8;i++){
+            if(x+i<8 && y-i>=0){
+                if (field[x+i][y-i] < 0 && this.color == 0) break;
+                if (field[x+i][y-i] > 0 && this.color == 1) break;
+                movelist.add(new Coordinate(x + i, y - i));
+                if (field[x+i][y-i] != 0) break;
+            }
+            else break;
+        }
+        for(int i=1;i<8;i++){
+            if(x-i<8 && y+i<8){
+                if (field[x-i][y+i] < 0 && this.color == 0) break;
+                if (field[x-i][y+i] > 0 && this.color == 1) break;
+                movelist.add(new Coordinate(x - i, y + i));
+                if (field[x-i][y+i] != 0) break;
+            }
+            else break;
+        }
+        for(int i=1;i<8;i++){
+            if(x+i<8 && y+i<8){
+                if (field[x-i][y-i] < 0 && this.color == 0) break;
+                if (field[x-i][y-i] > 0 && this.color == 1) break;
+                movelist.add(new Coordinate(x - i, y - i));
+                if (field[x-i][y-i] != 0) break;
+            }
+            else break;
+        }
+        return movelist;
+    }
 }
 
 class Pawn extends figures{
@@ -24,13 +102,13 @@ class Pawn extends figures{
     void move(int x, int y){
 
     }
-
+    @Override
     ArrayList check(int field[][]){
         ArrayList movelist = new ArrayList();
         int x = this.coor.getX();
         int y = this.coor.getY();
         if(this.color == 0){
-            if(this.coor.getY() == 1) if(field[x][y+2] == 0) movelist.add(new Coordinate(x,y+2));
+            if(y == 1) if(field[x][y+2] == 0) movelist.add(new Coordinate(x,y+2));
             if(field[x][y+1] == 0) movelist.add(new Coordinate(x,y+1));
             if(x-1>=0) if(field[x-1][y+1] > 0) movelist.add(new Coordinate(x-1,y+1));
             if(x+1<8) if(field[x+1][y+1] > 0) movelist.add(new Coordinate(x+1,y+1));
@@ -54,6 +132,13 @@ class Rook extends figures{
     void move(int x, int y){
 
     }
+    @Override
+    ArrayList check(int field[][]){
+        ArrayList movelist = new ArrayList();
+        int x = this.coor.getX();
+        int y = this.coor.getY();
+        return checkRook(field,x,y,movelist);
+    }
 }
 
 class Bishop extends figures{
@@ -64,6 +149,13 @@ class Bishop extends figures{
     @Override
     void move(int x, int y){
 
+    }
+    @Override
+    ArrayList check(int field[][]){
+        ArrayList movelist = new ArrayList();
+        int x = this.coor.getX();
+        int y = this.coor.getY();
+        return checkRook(field,x,y,movelist);
     }
 }
 
@@ -76,6 +168,13 @@ class Knight extends figures{
     void move(int x, int y){
 
     }
+    @Override
+    ArrayList check(int field[][]){
+        ArrayList movelist = new ArrayList();
+        int x = this.coor.getX();
+        int y = this.coor.getY();
+        return checkRook(field,x,y,movelist);
+    }
 }
 
 class Queen extends figures{
@@ -86,6 +185,14 @@ class Queen extends figures{
     @Override
     void move(int x, int y){
 
+    }
+    @Override
+    ArrayList check(int field[][]){
+        ArrayList movelist = new ArrayList();
+        int x = this.coor.getX();
+        int y = this.coor.getY();
+        movelist = checkRook(field,x,y,movelist);
+        return checkBishop(field,x,y,movelist);
     }
 }
 
